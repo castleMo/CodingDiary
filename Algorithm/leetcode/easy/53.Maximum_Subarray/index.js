8848d590a2157f5f14dee26c4c3718908ef9d760
castleMo <eodah4115@gmail.com>
2021-10-31 00:06:17 +0900

    Add Leetcode Problem number 53

--- Algorithm/leetcode/easy/53.Maximum_Subarray/index.js
+++ Algorithm/leetcode/easy/53.Maximum_Subarray/index.js
@@ -8,13 +8,12 @@
  const input = [];
  
  rl.on('line', (line) => {
-     input.push(Number(line));
      input.push(line);
  })
      .on('close', () => {
-         const [] = input;
+         const [nums] = input;
          console.log("\n");
-         const result = func();
+         const result = maxSubArray(JSON.parse(nums));
  
          console.log("\n");
          console.log({ result });
@@ -22,6 +21,16 @@
          process.exit();
      });
  
-const func = () => {
-    
+const maxSubArray = (nums) => {
+    let sum = nums[0];
+    let currentSum = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        if(currentSum < 0) {
+            currentSum = 0;
+        }
+        currentSum += nums[i];
+        sum = Math.max(sum, currentSum);
+    }
+    return sum;
 };
