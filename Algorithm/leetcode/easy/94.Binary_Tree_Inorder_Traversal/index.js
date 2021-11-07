19827422743e934cb8434cf7bca42285345539c3
castleMo <eodah4115@gmail.com>
2021-11-07 23:50:44 +0900

    Add Leetcode Problem 94

---
+++ Algorithm/leetcode/easy/94.Binary_Tree_Inorder_Traversal/index.js
@@ -0,0 +1,49 @@
+ const readline = require('readline');
+
+ const rl = readline.createInterface({
+     input: process.stdin,
+     output: process.stdout
+ });
+ 
+ const input = [];
+ 
+ rl.on('line', (line) => {
+     input.push(JSON.parse(line));
+ })
+     .on('close', () => {
+         const [arr] = input;
+         console.log("\n");
+         for(let i = 0; i < arr.length; i++) {
+
+         }
+         const result = inorderTraversal();
+ 
+         console.log("\n");
+         console.log({ result });
+ 
+         process.exit();
+     });
+
+const dfs = (root, result) => {
+    if(root != null) {
+        dfs(root.left, result);
+        result.push(root.val);
+        dfs(root.right, result);
+    }
+}
+ 
+const inorderTraversal = (root) => {
+    let result = [];
+
+    dfs(root, result);
+    
+    return result;
+};
+
+class TreeNode {
+    constructor(val, left, right) {
+        this.val = (val === undefined ? 0 : val);
+        this.left = (left === undefined ? null : left);
+        this.right = (right === undefined ? null : right);
+    }
+}
