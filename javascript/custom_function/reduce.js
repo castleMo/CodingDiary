84b589600f259b62b5050a28429fcc7f135c1249
강성모 <eodah4115@gmail.com>
2021-11-26 16:50:40 +0900

    Add Javascript Custom Function

--- javascript/custom_function/reduce.js
+++ javascript/custom_function/reduce.js
@@ -1,4 +1,27 @@
+const arr = [];
 
-const customReduce = (callback, initialValue) => {
-
+for(let i = 0; i < Math.pow(10,5); i++) {
+    arr.push(i);
 }
+
+Array.prototype.customReduce = function(callbackfn, initialValue) {
+    for(let i = 0; i < this.length; i++) {
+        initialValue = callbackfn(initialValue, this[i], i, this);
+    }
+
+    return initialValue;
+}
+
+console.time('reduce');
+const result1 = arr.reduce((prev, curr, currIndex, array) => {
+    return prev + curr;
+}, 0);
+console.timeEnd('reduce');
+
+console.time('customReduce');
+const result2 = arr.customReduce((prev, curr, currIndex, array) => {
+    return prev + curr;
+}, 0);
+console.timeEnd('customReduce');
+
+console.log({result1, result2});
