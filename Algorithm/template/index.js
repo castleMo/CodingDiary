94fb717b95638c65c81b5fa4c3844e52bfedf715
castlemo <eodah4115@gmail.com>
2021-12-05 21:52:17 +0900

    Update template

--- Algorithm/template/index.js
+++ Algorithm/template/index.js
@@ -1,28 +1,28 @@
- const readline = require('readline');
+const readline = require('readline');
 
- const rl = readline.createInterface({
-     input: process.stdin,
-     output: process.stdout
- });
- 
- const input = [];
- 
- rl.on('line', (line) => {
-     input.push(Number(line));
-     input.push(line);
-     input.push(JSON.parse(line));
-    })
-     .on('close', () => {
-         const [] = input;
-         console.log("\n");
-         const result = func();
- 
-         console.log("\n");
-         console.log({ result });
- 
-         process.exit();
-     });
- 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const input = [];
+
+rl.on('line', (line) => {
+    input.push(Number(line));
+    input.push(line);
+    input.push(JSON.parse(line));
+})
+    .on('close', () => {
+        const [] = input;
+        console.log("\n");
+        const result = func();
+
+        console.log("\n");
+        console.log({ result });
+
+        process.exit();
+    });
+
 const func = () => {
     
 };
