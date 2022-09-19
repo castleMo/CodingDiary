8f291a55c45c971c81136e8434cdfd1aab110789
castlemo <eodah4115@gmail.com>
2022-09-19 12:57:04 +0900

    Solved baekjoon problem 2754

---
+++ algorithm/baekjoon/27XX/2754/index.js
@@ -0,0 +1,59 @@
+/**
+ * @link https://www.acmicpc.net/problem/2754
+ */
+const readline = require('readline');
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+const input = [];
+
+rl.on('line', (line) => {
+	input.push(line);
+}).on('close', () => {
+	switch (input[0]) {
+		case 'A+':
+			console.log('4.3');
+			break;
+		case 'A0':
+			console.log('4.0');
+			break;
+		case 'A-':
+			console.log('3.7');
+			break;
+		case 'B+':
+			console.log('3.3');
+			break;
+		case 'B0':
+			console.log('3.0');
+			break;
+		case 'B-':
+			console.log('2.7');
+			break;
+		case 'C+':
+			console.log('2.3');
+			break;
+		case 'C0':
+			console.log('2.0');
+			break;
+		case 'C-':
+			console.log('1.7');
+			break;
+		case 'D+':
+			console.log('1.3');
+			break;
+		case 'D0':
+			console.log('1.0');
+			break;
+		case 'D-':
+			console.log('0.7');
+			break;
+		default:
+			console.log('0.0');
+			break;
+	}
+
+	process.exit();
+});
