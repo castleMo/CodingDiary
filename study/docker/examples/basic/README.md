ca7655e16e02156c075fa7ed730315b1889362ec
seongmo <eodah4115@gmail.com>
2023-01-12 11:00:45 +0900

    docker.md

---
+++ study/docker/examples/basic/README.md
@@ -0,0 +1,4 @@
+## Docker Build And Run
+
+1. docker build --tag {image-name} .
+2. docker run -dp {local-port}:{docker-port} --name {container-name} {image-name}  
---
+++ study/docker/examples/basic/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+// app.use('/', (req, res, next) => {
+// 	console.log('Hello');
+// 	res.send('Hello World');
+// });
+//
+// app.listen(8000, () => {
+// 	console.log('Server Start');
+// });
+
+console.log('sv start');
+console.log(1);
+console.log(2);
+console.log(3);
+console.log('sv stop');
